09e3e177893c808fdc55430836fa4a4645f64d4a
Muhibur Rahman <muhib5532@gmail.com>
2022-03-02 16:31:49 +0600

    NFT website startup code file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "remixicon/fonts/remixicon.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
