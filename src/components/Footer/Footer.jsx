d6e01af82d7a24ed651d0ffd5783a249a07c0d41
Folarin Lawal <lawalfolarin6@gmail.com>
2022-05-26 12:30:23 +0000

    Footer Copyright

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -151,7 +151,7 @@ const Footer = () => {
           <Col lg="12" className=" mt-4 text-center">
             <p className="copyright">
               {" "}
-              Copyrights 2022, Developed by Muhibur Rahman. @Muhib's Tech Diary
+              Copyrights 2022, NFT MarketPlace
               All Rights Reserved.{" "}
             </p>
           </Col>
